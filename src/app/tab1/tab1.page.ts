836405de645a98c28a900ec726125effa50959a4
LautaroViceconti <lautaro.viceconti@gmail.com>
2022-10-24 04:21:57 -0300

    initialCommit

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuController} from '@ionic/angular';
+
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page implements OnInit {
+  
+
+  constructor(private menuCtrl:MenuController) {}
+
+  onClick(){
+    this.menuCtrl.toggle()
+  }
+
+  ngOnInit() {
+  }
+
+}
+
